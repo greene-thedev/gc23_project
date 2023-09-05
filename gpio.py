cde449acfdaa7d0e7b47207102cf49ad19f22000
Jeremiah Greene <jerrygreene010@gmail.com>
2023-09-05 13:29:21 +0000

    Implemented wait time on a button press

--- gpio.py
+++ gpio.py
@@ -1,34 +1,43 @@
-from gpiozero import LED, Button
+from gpiozero import LED, Button, Buzzer
 from signal import pause
 from time import sleep
 # from Adafruit_ADS1x15 import ADS1x15
 
 button = Button(2)
-flame_detector = ADS1x15()
+buzzer = Buzzer(3)
+# flame_detector = ADS1x15()
 
 red = LED(15)
 yellow = LED(24)
 green = LED(18)
 
+# while True:
+#     green.on()
+#     if flame_detector:
+
+green.off()
+for i in range(0,5):
+    buzzer.on()
+    sleep(1)
+    buzzer.off()
+    yellow.on()
+    sleep(1)
+    yellow.off()
+    
+
 while True:
-    green.on()
-    if flame_detector:
-        green.off()
-        yellow.on()
-        sleep(2)
-        yellow.off()
-        while True:
-            red.on()
-            sleep(0.1)
-            red.off()
-            sleep(0.1)
-            if button.is_held:
-                break
-
-
-
-
-
-
+    buzzer.on()
+    # buzzer.blink
+    sleep(0.05)
+    buzzer.off()
+    red.on()
+    sleep(0.1)
+    red.off()
+    button._hold_time = 5
+    if button.is_held:
+        break
+  
+     
+        
 
 pause()
