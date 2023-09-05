74baddb74e4e48fbce7c5585888371a7cbf6b16c
Jeremiah Greene <jerrygreene010@gmail.com>
2023-09-05 11:20:26 +0000

    added logic to button

--- gpio.py
+++ gpio.py
@@ -3,15 +3,31 @@ from signal import pause
 from time import sleep
 # from Adafruit_ADS1x15 import ADS1x15
 
-
+button = Button(2)
+flame_detector = ADS1x15()
 
 red = LED(15)
 yellow = LED(24)
 green = LED(18)
 
-green.on()
-yellow.on()
-red.on()
+while True:
+    green.on()
+    if flame_detector:
+        green.off()
+        yellow.on()
+        sleep(2)
+        yellow.off()
+        while True:
+            red.on()
+            sleep(0.1)
+            red.off()
+            sleep(0.1)
+            if button.is_held:
+                break
+
+
+
+
 
 
 
